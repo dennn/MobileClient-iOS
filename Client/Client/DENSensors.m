9bf631acc8942092e3a9d7af45c27df46156ef9f
Denis Ogun <do1303@my.bristol.ac.uk>
2014-02-26 15:33:43 +0000

    Invert the accelerometer values for iOS

--- Client/Client/DENSensors.m
+++ Client/Client/DENSensors.m
@@ -75,9 +75,9 @@
         {
             NSMutableDictionary *sensorDictionary = [NSMutableDictionary new];
             CMAccelerometerData *accelerometerData = self.sensorManager.accelerometerData;
-            [sensorDictionary setObject:[NSNumber numberWithDouble:(accelerometerData.acceleration.x*9.81)] forKey:@"X"];
-            [sensorDictionary setObject:[NSNumber numberWithDouble:(accelerometerData.acceleration.y*9.81)] forKey:@"Y"];
-            [sensorDictionary setObject:[NSNumber numberWithDouble:(accelerometerData.acceleration.z*9.81)] forKey:@"Z"];
+            [sensorDictionary setObject:[NSNumber numberWithDouble:(accelerometerData.acceleration.x*9.81*-1)] forKey:@"X"];
+            [sensorDictionary setObject:[NSNumber numberWithDouble:(accelerometerData.acceleration.y*9.81*-1)] forKey:@"Y"];
+            [sensorDictionary setObject:[NSNumber numberWithDouble:(accelerometerData.acceleration.z*9.81*-1)] forKey:@"Z"];
             return sensorDictionary;
         }
             
